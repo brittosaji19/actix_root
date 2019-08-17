4b8b375fe7f67de1514d9e340f081d3214d09a21
brittosaji <bs1067@gmail.com>
2019-08-17 12:44:02 +0530

    Comments on controller functions

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -8,8 +8,11 @@ pub struct User {
 }
 
 pub fn ack() -> impl Responder {
-    HttpResponse::Ok().body("Hello from auth controller")
+    HttpResponse::Ok().body("Welcome to auth controller")
 }
+
+//Basic signup function that should return a JSON response containing a success status and an access_token
+//TODO full functionality not yet implemented
 pub fn signup() -> impl Responder {
     //TEMPORARY IMPLEMENTATION FOR TESTING
     let hash_response = encrypt::generate_hash("hello".to_string());
@@ -20,7 +23,15 @@ pub fn signup() -> impl Responder {
         hash_response, hash_verify
     ))
 }
+//Basic login function that should return a JSON response containing a success status and an access_token
+//TODO full functionality not yet implemented
+pub fn login() -> impl Responder {
+    HttpResponse::Ok().body(format!("You have reached the login endpoint",))
+}
 
+//Creates a JSON Web Token byt taking a user id as a parameter and creating a User Struct with id value that was passed into the function
+//The default expiry is set to be 1 hour
+//Default headers are userd to encode however a reference to a JWTSECRET is supplied as key for token generation
 pub fn create_and_register_token(id: &str) -> String {
     let expiry = std::time::SystemTime::now()
         .checked_add(std::time::Duration::new(3600, 0))
@@ -39,6 +50,9 @@ pub fn create_and_register_token(id: &str) -> String {
     token.unwrap()
 }
 
+// Decodes the incoming token &str to generate the User Struct that was encoded into the token
+// A leeway of 60 seconds is added to adjust for any possible clock skew while checking for token expiry
+//The JWTSECRET used while generating the token is supplied as key to accurately decrypt the token
 pub fn decode_token(
     token: &str,
 ) -> Result<jsonwebtoken::TokenData<User>, jsonwebtoken::errors::Error> {
@@ -52,6 +66,5 @@ pub fn decode_token(
             ..Default::default()
         },
     );
-    // let token_data=jsonwebtoken::dangerous_unsafe_decode::<User>(&token);
     token_data
 }
--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -5,14 +5,9 @@ use actix_web::{web, HttpResponse};
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/auth")
-            //TEMPORARILY DISABLED FOR JWT IMPLEMENTATION
-            .wrap(middlewares::authenticate::Authenticator)
-            .route(
-                "/login",
-                web::get().to(|| HttpResponse::Ok().body("Welcome to Login route")),
-            )
+            .route("/login", web::post().to(controllers::auth::login))
             .route("/signup", web::post().to(controllers::auth::signup))
-            .route("/ack", web::get().to(controllers::auth::ack))
+            .route("/", web::get().to(controllers::auth::ack))
             .route("", web::get().to(controllers::auth::ack)),
     );
 }
